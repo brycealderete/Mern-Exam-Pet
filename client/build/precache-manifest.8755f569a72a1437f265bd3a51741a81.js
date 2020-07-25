53f266a1a34cb087240a7fcc83c534f31404ae12
Bryce Alderete <brycealderete@gmail.com>
2020-07-25 08:15:01 -0700

    first commit

---
+++ client/build/precache-manifest.8755f569a72a1437f265bd3a51741a81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e050680c230614ff30aef83c7b5fc39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb6ff28a78f32e448c3a",
+    "url": "/static/css/main.b5ec88bc.chunk.css"
+  },
+  {
+    "revision": "ee83ac334e9ad87b04bb",
+    "url": "/static/js/2.6e179b21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6e179b21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb6ff28a78f32e448c3a",
+    "url": "/static/js/main.89fe73ab.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
